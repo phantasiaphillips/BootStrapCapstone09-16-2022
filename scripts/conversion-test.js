6c5e707931dd820bdb454a000d35bb4800981ffd
phantasiaphillips <pphillips@nyc.yearup.org>
2022-10-19 09:13:13 -0400

    con

---
+++ scripts/conversion-test.js
@@ -0,0 +1,10 @@
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+console.log(parseFloat(a));
+console.log(parseFloat(b));
+console.log(parseFloat(c));
+console.log(parseFloat(d));
+
---
+++ scripts/hello.js
@@ -0,0 +1,18 @@
+// Hello World
+
+"use strict"
+
+var message="hello world";
+
+var firstName = "phantasia";
+var lastName = "phillips";
+
+var fullName = lastName + ", " + firstName;
+console.log(fullName);
+
+console.log(lastName + ", " + firstName);
+
+var num1 = ;
+var num2 = ;
+var answer1 = num1 + num2;
+
