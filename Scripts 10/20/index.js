5c783ce367f3ab07c633bff8b1a8935054957707
phantasiaphillips <pphillips@nyc.yearup.org>
2022-10-20 13:59:36 -0400

    calcultor

---
+++ Scripts 10/20/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+window.onload = init;
+
+function init() {
+ 
+    const helloBtn = document.getElementById("helloBtn");
+ 
+    helloBtn.onclick = onHelloBtnClicked;
+}
+
+function onHelloBtnClicked() {
+ alert("Hi there!");
+}
+console.log("I just created an alert!")
+//alert("Hi there!");
+    const thePara = document.getElementById("thePara");
+    thePara
+
+
